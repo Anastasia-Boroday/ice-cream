dbcd73aceb4dfeaac0f08ac6aee22908e9f0088f
Anastasia-Boroday <anastasiaborodaj22@gmail.com>
2022-01-19 19:29:15 +0200

    add favicon

--- src/scripts/mobile.js
+++ src/scripts/mobile.js
@@ -10,8 +10,7 @@
         openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
         mobileMenu.classList.toggle("is-open");
 
-        const scrollLockMethod = !isMenuOpen ? "disableBodyScroll" : "enableBodyScroll";
-        bodyScrollLock[scrollLockMethod](document.body);
+     
     };
 
     openMenuBtn.addEventListener("click", toggleMenu);
