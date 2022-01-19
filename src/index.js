3872d352dd106d76dabef06e3558e0f3e42cbfe6
NataliaPavlovskaya <91052774+NataliaPavlovskaya@users.noreply.github.com>
2022-01-19 16:23:22 +0200

    good job

--- src/index.js
+++ src/index.js
@@ -5,5 +5,7 @@ import './scripts/customer/slider.js';
 
 import './scripts/mobile.js';
 import './scripts/btn-back';
+import './scripts/products-mod'
 
-
+import './scripts/products-mod2'
+import './scripts/products-mod3'
